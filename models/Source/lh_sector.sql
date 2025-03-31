b3bb210dc81d4e3274478045e48a0a4e771186c1
JanNeoV <142179566+JanNeoV@users.noreply.github.com>
2025-03-31 22:07:47 +0200

    feat: add missing tables

--- models/Source/lh_sector.sql
+++ models/Source/lh_sector.sql
@@ -1,1 +1,5 @@
-SELECT * FROM [LH_EUROSTAT].[PublicFinance].[dim_sector]
+SELECT 
+    {{ generate_tsql_surrogate_key_integer(["dimension_code"], 3) }} AS pk
+    , dimension_code as sector_code
+    , dimension_label as sector_label
+FROM [LH_EUROSTAT].[PublicFinance].[dim_sector]
--- models/Source/lh_unit.sql
+++ models/Source/lh_unit.sql
@@ -1,1 +1,5 @@
-SELECT * FROM [LH_EUROSTAT].[PublicFinance].[dim_unit]
+SELECT 
+    {{ generate_tsql_surrogate_key_integer(["dimension_code"], 3) }} AS pk
+    , dimension_code as unit_code
+    , dimension_label as unit_label
+FROM [LH_EUROSTAT].[PublicFinance].[dim_unit]
