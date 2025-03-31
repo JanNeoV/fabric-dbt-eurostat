cc8deb1218a0ca5a6fff220f34fb6abffa8aed47
JanNeoV <142179566+JanNeoV@users.noreply.github.com>
2025-03-31 22:24:03 +0200

    feat: adjust initial setup

--- models/Source/lh_na_item.sql
+++ models/Source/lh_na_item.sql
@@ -1,1 +1,5 @@
-SELECT * FROM [LH_EUROSTAT].[PublicFinance].[dim_na_item]
+SELECT 
+    {{ generate_tsql_surrogate_key_integer(["dimension_code"], 3) }} AS pk
+    , dimension_code as na_code
+    , dimension_label as na_label
+ FROM [LH_Eurostat].[PublicFinance].[dim_na_item]
